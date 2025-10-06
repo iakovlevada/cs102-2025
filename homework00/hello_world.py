1bd95311298d68596c96a67d28ce3a7b0b5a6b9c
mac <mac@MacBook-Air-mac.local>
2025-10-06 17:37:12 +0300

    Исправлены ошибки 2

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,10 +1,14 @@
 """
 Тест
 """
+
+
 def text(o):
     """Возвращает 'message' или 'error'"""
-    if o>float('-inf'):
-        return 'message'
-    return 'error'
-A=5
+    if o > float("s-inf"):
+        return "message"
+    return "error"
+
+
+A = 5
 print(text(A))
