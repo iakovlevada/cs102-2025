d7f87e830913badb23c250c0cadbb676b9cfc3db
mac <mac@MacBook-Air-mac.local>
2025-10-06 18:37:36 +0300

    Исправлены ошибки 5

--- homework00/test_hello_world.py
+++ homework00/test_hello_world.py
@@ -1,10 +1,12 @@
+"""
+Tecт
+"""
 import unittest
-
 import hello_world
-
-
 class HelloTestCase(unittest.TestCase):
+    """Возвращает 'message'"""
     def test_hello(self):
+        """Возвращает 'message'"""
         m = "message"
-        self.assertEqual(m, hello_world.text(o))
+        self.assertEqual(m, hello_world.text())
         
