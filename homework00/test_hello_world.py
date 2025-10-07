aa2d30f6e6d8cf6bfcbed5f70237e47862c0332f
iakovlevada <dariayakovleva5@mail.ru>
2025-10-07 17:22:53 +0300

    Исправлены ошибки 6

--- homework00/test_hello_world.py
+++ homework00/test_hello_world.py
@@ -1,12 +1,16 @@
 """
 Tecт
 """
+
 import unittest
+
 import hello_world
+
+
 class HelloTestCase(unittest.TestCase):
     """Возвращает 'message'"""
+
     def test_hello(self):
         """Возвращает 'message'"""
         m = "message"
         self.assertEqual(m, hello_world.text())
-        
