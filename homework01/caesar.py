cc9f967cccba7e88b3c464091331524033436729
iakovlevada <dariayakovleva5@mail.ru>
2025-10-30 11:16:12 +0300

    Реализованы функции encrypt_caesar() и decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,3 +1,8 @@
+"""
+Caesar cipher
+"""
+
+
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -11,7 +16,15 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for char in plaintext:
+        if ord(char) in range(ord("Z") + 1 - shift, ord("Z") + 1):
+            ciphertext += chr(ord(char) - (ord("Z") - ord("A") + 1) + shift)
+        elif ord(char) in range(ord("z") + 1 - shift, ord("z") + 1):
+            ciphertext += chr(ord(char) - (ord("z") - ord("a") + 1) + shift)
+        elif not char.isalpha():
+            ciphertext += char
+        else:
+            ciphertext += chr(ord(char) + shift)
     return ciphertext
 
 
@@ -28,5 +41,18 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for char in ciphertext:
+        if ord(char) in range(ord("A"), ord("A") + shift):
+            plaintext += chr(ord(char) + (ord("Z") - ord("A") + 1) - shift)
+        elif ord(char) in range(ord("a"), ord("a") + shift):
+            plaintext += chr(ord(char) + (ord("z") - ord("a") + 1) - shift)
+        elif not char.isalpha():
+            plaintext += char
+        else:
+            plaintext += chr(ord(char) - shift)
     return plaintext
+
+
+STR = str(input())
+print(encrypt_caesar(STR))
+print(decrypt_caesar(STR))
