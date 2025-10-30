4d148caf8f808e73046dbdeee3471369260159b7
iakovlevada <dariayakovleva5@mail.ru>
2025-10-30 11:58:51 +0300

    Реализованы функции encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,8 @@
+"""
+Vigenere cipher
+"""
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,7 +14,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    alphabet = [chr(A) for A in range(ord("A"), ord("Z") + 1)]
+    j = 0
+    for char in plaintext:
+        for k, letter in enumerate(alphabet):
+            if keyword[j] == letter or keyword[j] == letter.lower():
+                if ord(char) in range(ord("Z") - k + 1, ord("Z") + 1):
+                    ciphertext += chr(ord(char) - (ord("Z") - ord("A") + 1) + k)
+                elif ord(char) in range(ord("z") - k + 1, ord("z") + 1):
+                    ciphertext += chr(ord(char) - (ord("z") - ord("a") + 1) + k)
+                elif not char.isalpha():
+                    ciphertext += char
+                else:
+                    ciphertext += chr(ord(char) + k)
+        j += 1
+        if j == len(keyword):
+            j = 0
     return ciphertext
 
 
@@ -24,5 +44,25 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    alphabet = [chr(A) for A in range(ord("A"), ord("Z") + 1)]
+    j = 0
+    for char in ciphertext:
+        for k, letter in enumerate(alphabet):
+            if keyword[j] == letter or keyword[j] == letter.lower():
+                if ord(char) in range(ord("A"), ord("A") + k):
+                    plaintext += chr(ord(char) + (ord("Z") - ord("A") + 1) - k)
+                elif ord(char) in range(ord("a"), ord("a") + k):
+                    plaintext += chr(ord(char) + (ord("z") - ord("a") + 1) - k)
+                elif not char.isalpha():
+                    plaintext += char
+                else:
+                    plaintext += chr(ord(char) - k)
+        j += 1
+        if j == len(keyword):
+            j = 0
     return plaintext
+
+
+STR, key = input().split()
+print(encrypt_vigenere(STR, key))
+print(decrypt_vigenere(STR, key))
