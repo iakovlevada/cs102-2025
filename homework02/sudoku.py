45a9b06b77b8a2635c297870e90d669f97afaea0
iakovlevada <dariayakovleva5@mail.ru>
2025-11-13 13:01:39 +0300

    Реализованы функции get_row(), get_col() и get_block()

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -5,7 +5,9 @@ T = tp.TypeVar("T")
 
 
 def read_sudoku(path: tp.Union[str, pathlib.Path]) -> tp.List[tp.List[str]]:
-    """Прочитать Судоку из указанного файла"""
+    """
+    Прочитать Судоку из указанного файла
+    """
     path = pathlib.Path(path)
     with path.open() as f:
         puzzle = f.read()
@@ -19,7 +21,9 @@ def create_grid(puzzle: str) -> tp.List[tp.List[str]]:
 
 
 def display(grid: tp.List[tp.List[str]]) -> None:
-    """Вывод Судоку"""
+    """
+    Вывод Судоку
+    """
     width = 2
     line = "+".join(["-" * (width * 3)] * 3)
     for row in range(9):
@@ -30,6 +34,9 @@ def display(grid: tp.List[tp.List[str]]) -> None:
 
 
 def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
+    """
+    Сгруппировать значения values в список, состоящий из списков по n элементов
+    """
     M = []
     m = []
     len_values = len(values)
@@ -44,40 +51,59 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
-    """Возвращает все значения для номера строки, указанной в pos
-    >>> get_row([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
-    ['1', '2', '.']
-    >>> get_row([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']], (1, 0))
-    ['4', '.', '6']
-    >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
-    ['.', '8', '9']
     """
-    pass
+    Возвращает все значения для номера строки, указанной в pos
+    """
+    row, col = pos
+    return grid[row]
 
 
 def get_col(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
-    """Возвращает все значения для номера столбца, указанного в pos
-    >>> get_col([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
-    ['1', '4', '7']
-    >>> get_col([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']], (0, 1))
-    ['2', '.', '8']
-    >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
-    ['3', '6', '9']
     """
-    pass
+    Возвращает все значения для номера столбца, указанного в pos
+    """
+    row, col = pos
+    C=[]
+    for g in grid:
+        C.append(g[col])
+    return C
 
 
 def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
-    """Возвращает все значения из квадрата, в который попадает позиция pos
-    >>> grid = read_sudoku('puzzle1.txt')
-    >>> get_block(grid, (0, 1))
-    ['5', '3', '.', '6', '.', '.', '.', '9', '8']
-    >>> get_block(grid, (4, 7))
-    ['.', '.', '3', '.', '.', '1', '.', '.', '6']
-    >>> get_block(grid, (8, 8))
-    ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    pass
+    Возвращает все значения из квадрата, в который попадает позиция pos
+    """
+    row, col = pos
+    R=[]
+    if 0 <= row <= 2:
+        for i in range(3):
+            r=get_row(grid, (i, 0))
+            if 0 <= col <= 2:
+                R.append(r[:3])
+            elif 3 <= col <= 5:
+                R.append(r[3:6])
+            elif 6 <= col <= 8:
+                R.append(r[6:9])
+    elif 3 <= row <= 5:
+        for i in range(3, 6):
+            r=get_row(grid, (i, 0))
+            if 0 <= col <= 2:
+                R.append(r[:3])
+            elif 3 <= col <= 5:
+                R.append(r[3:6])
+            elif 6 <= col <= 8:
+                R.append(r[6:9])
+    elif 6 <= row <= 9:
+        for i in range(6, 9):
+            r=get_row(grid, (i, 0))
+            if 0 <= col <= 2:
+                R.append(r[:3])
+            elif 3 <= col <= 5:
+                R.append(r[3:6])
+            elif 6 <= col <= 8:
+                R.append(r[6:9])
+    B=sum(R, [])
+    return B
 
 
 def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[int, int]]:
