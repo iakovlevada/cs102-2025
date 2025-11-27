ae375f5815f4b17185ddc78c591396bf70096e6f
iakovlevada <dariayakovleva5@mail.ru>
2025-11-27 15:46:23 +0300

    Исправлена графическая реализация лабиринта

--- homework03/maze_gui.py
+++ homework03/maze_gui.py
@@ -1,7 +1,8 @@
 import tkinter as tk
+from tkinter import messagebox, ttk
 from typing import List
-from tkinter import ttk, messagebox
-from maze import bin_tree_maze, solve_maze, add_path_to_grid
+
+from maze import add_path_to_grid, bin_tree_maze, solve_maze
 
 
 def draw_cell(x, y, color, size: int = 10):
@@ -16,21 +17,22 @@ def draw_maze(grid: List[List[str]], size: int = 10):
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
             if cell == " ":
-                color = 'White'
+                color = "White"
             elif cell == "■":
-                color = 'black'
+                color = "black"
             elif cell == "X":
-                color = "red"
+                color = "purple"
             draw_cell(y, x, color, size)
 
 
 def show_solution():
+    global GRID, N, M
     maze, path = solve_maze(GRID)
+    while not path:
+        GRID = bin_tree_maze(N, M)
+        maze, path = solve_maze(GRID)
     maze = add_path_to_grid(GRID, path)
-    if path:
-        draw_maze(maze, CELL_SIZE)
-    else:
-        tk.messagebox.showinfo("Message", "No solutions")
+    draw_maze(maze, CELL_SIZE)
 
 
 if __name__ == "__main__":
@@ -41,7 +43,7 @@ if __name__ == "__main__":
     GRID = bin_tree_maze(N, M)
 
     window = tk.Tk()
-    window.title('Maze')
+    window.title("Maze")
     window.geometry("%dx%d" % (M * CELL_SIZE + 100, N * CELL_SIZE + 100))
 
     canvas = tk.Canvas(window, width=M * CELL_SIZE, height=N * CELL_SIZE)
@@ -51,4 +53,3 @@ if __name__ == "__main__":
     ttk.Button(window, text="Solve", command=show_solution).pack(pady=20)
 
     window.mainloop()
-
